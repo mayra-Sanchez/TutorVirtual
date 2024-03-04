368d49d1a04be47ed27902b237cc2c7ee360efb1
Mayra Sanchez <mayra.alejandra.sanchez@correounivalle.edu.co>
2024-03-04 14:45:42 -0500

    add changes in courseCard.js

--- Front/tutor-virtual/src/components/courseCard.js
+++ Front/tutor-virtual/src/components/courseCard.js
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AiOutlineClose } from "react-icons/ai";
-import "./CourseCard.css";
 import { Link } from "react-router-dom";
+import "./CourseCard.css";
+import { listCourses } from "../Services/Course";
 
-const CourseCard = ({ name, teacher, creationDate, description }) => {
+const CourseCard = ({ courseId }) => {
+  const [course, setCourse] = useState(null);
   const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    const fetchCourse = async () => {
+      try {
+        const courses = await listCourses();
+        const selectedCourse = courses.find((c) => c.id === courseId);
+        setCourse(selectedCourse);
+      } catch (error) {
+        console.error("Error fetching course:", error);
+      }
+    };
+
+    fetchCourse();
+  }, [courseId]);
 
   const openModal = () => {
     setShowModal(true);
@@ -16,13 +32,15 @@ const CourseCard = ({ name, teacher, creationDate, description }) => {
 
   return (
     <>
-      <div className="card" onClick={openModal}>
-        <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-          <h6 className="card-subtitle mb-2 text-muted">{teacher}</h6>
-          <p className="card-text">Creado: {creationDate}</p>
+      {course && (
+        <div className="card" onClick={openModal}>
+          <div className="card-body">
+            <h5 className="card-title">{course.name}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">{course.instructor_name}</h6>
+            <p className="card-text">Creado: {course.creation_date}</p>
+          </div>
         </div>
-      </div>
+      )}
 
       {showModal && (
         <div className="modal-overlay" onClick={closeModal}>
@@ -30,10 +48,10 @@ const CourseCard = ({ name, teacher, creationDate, description }) => {
             <button className="close-btn" onClick={closeModal}>
               <AiOutlineClose />
             </button>
-            <h2>{name}</h2>
-            <h3>{teacher}</h3>
-            <p>Creado: {creationDate}</p>
-            <p>{description}</p>
+            <h2>{course.name}</h2>
+            <h3>{course.instructor_name}</h3>
+            <p>Creado: {course.creation_date}</p>
+            <p>{course.description}</p>
             <Link to="/Tutor" className="ask-btn">
               Preguntale al tutor
             </Link>
