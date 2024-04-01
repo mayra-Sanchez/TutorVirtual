0ff80702b42b23a918ded36f5421a7fb4750eff5
SantiagoCasanas <santiago01.02.003@gmail.com>
2024-03-31 19:19:20 -0500

    correr el proyecto localmente

--- Back/Course/urls.py
+++ Back/Course/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('/home', home, name='home'),
-    path('/list', List.as_view(), name='list'),
-    path('/teacher/register', Register.as_view(), name='register'),
-    path('/student/<int:pk>/chat', Chat.as_view(), name='chat')
+    path('home', home, name='home'),
+    path('list', List.as_view(), name='list'),
+    path('teacher/register', Register.as_view(), name='register'),
+    path('student/<int:pk>/chat', Chat.as_view(), name='chat')
 ]
